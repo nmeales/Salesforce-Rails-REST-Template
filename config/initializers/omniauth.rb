fee68d2724ceed0cba34b5c5d6075c784d9f1541
Nick Eales <n.m.eales@gmail.com>
2013-04-11 13:10:09 +0100

    More SSl Changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,8 @@ require 'forcedotcom'
 # Set the default hostname for omniauth to send callbacks to.
 # seems to be a bug in omniauth that it drops the httpS
 # this still exists in 0.2.0
-OmniAuth.config.full_host = 'https://localhost:3000'
+# OmniAuth.config.full_host = 'https://localhost:3000'
+OmniAuth.config.full_host = 'https://peaceful-badlands-9510.herokuapp.com/'
 # OmniAuth.config.full_host = 'https://telesales-rails.heroku.com'
 
 module OmniAuth
