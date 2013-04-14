08f113d3842f2e7087669653fdbbf4d1afaf29a4
Nick Eales <n.m.eales@gmail.com>
2013-04-14 10:10:54 +0100

    Facebook Session Problem

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -15,13 +15,13 @@ class StoreController < ApplicationController
   def index
     Salesforce.set_http(session[:accesstoken], session[:accessurl])
     @user = Salesforce.get_user()
-    # if @user.parsed_response[0].has_key?("errorCode") unless @user.parsed_response[0].nil?
-    #   # reset_session
-    #   redirect_to :controller => 'store', :action => 'login'
-    # else
-    #   @branding = Salesforce.get_template(@user["id"])
-    #   @products = Salesforce.get_products()
-    # end
+    unless @user.parsed_response[0].nil?
+      # reset_session
+      redirect_to :controller => 'store', :action => 'login'
+    else
+      @branding = Salesforce.get_template(@user["id"])
+      @products = Salesforce.get_products()
+    end
   end
 
   # create the order in salesforce
